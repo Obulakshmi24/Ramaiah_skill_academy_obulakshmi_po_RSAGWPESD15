140933fd5a374f3d9857e3375c3aaff78c7c14e9
Obulakshmi24 <obulakshmi2407@gmail.com>
2024-10-27 12:19:42 +0530

    Create lab_pro_1.m

---
+++ lab_pro_1.m
@@ -0,0 +1,5 @@
+% MATLAB Programming
+% Develop a MATLAB script that simulates and plots the trajectory of a projectile. Take 
+% into account initial velocity, launch angle, and gravitational acceleration. Plot the 
+% range and maximum height. 
+
